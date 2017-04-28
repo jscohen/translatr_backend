3c5cca7e196a2259c6098262cdbff1a579070fc6
jscohen <jon.cohen08@gmail.com>
2017-04-28 09:38:49 -0400

    Added artist relationship to songs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170427202457) do
+ActiveRecord::Schema.define(version: 20170428133254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,8 @@ ActiveRecord::Schema.define(version: 20170427202457) do
     t.string   "album"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "artist_id"
+    t.index ["artist_id"], name: "index_songs_on_artist_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -78,5 +80,6 @@ ActiveRecord::Schema.define(version: 20170427202457) do
   add_foreign_key "albums", "users", name: "albums_user_id_fkey"
   add_foreign_key "artists", "users"
   add_foreign_key "examples", "users"
+  add_foreign_key "songs", "artists"
   add_foreign_key "users", "albums"
 end
--- scripts/create-song.sh
+++ scripts/create-song.sh
@@ -2,7 +2,7 @@ curl --include --request POST http://localhost:4741/songs \
   --header "Content-Type: application/json" \
   --data '{
     "song": {
-      "name": "Inta Daiman",
-      "album": "20"
+      "name": "La Solitudine",
+      "album": "Laura Pausini"
     }
   }'
