c7b82fe78dbb8201f1f4b2d59e46fe4cfe9c5118
jscohen <jon.cohen08@gmail.com>
2017-05-08 16:02:36 -0400

    Updated song script

--- scripts/update-song.sh
+++ scripts/update-song.sh
@@ -1,11 +1,11 @@
-curl --include --request PATCH http://localhost:4741/songs/5 \
+curl --include --request PATCH http://localhost:4741/songs/25 \
   --header "Content-Type: application/json" \
   --header "Authorization: Token token=${TOKEN}" \
   --data '{
     "song": {
-      "name": "Inta Daiman",
-      "album": "20",
-      "artist_id": "1",
+      "name": "Inta Daimanman",
+      "album": "19",
+      "artist_id": "50",
       "user_id": "1"
     }
   }'
