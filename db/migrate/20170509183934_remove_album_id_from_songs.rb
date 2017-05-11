1d242cceff2a1a6a665c760dcb75490dc2f7d20c
jscohen <jon.cohen08@gmail.com>
2017-05-11 11:34:29 -0400

    Removing bad migrations

--- db/migrate/20170509183934_remove_album_id_from_songs.rb
+++ db/migrate/20170509183934_remove_album_id_from_songs.rb
@@ -1,5 +1,5 @@
-class RemoveAlbumIdFromSongs < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :songs, :album_id
-  end
-end
+# class RemoveAlbumIdFromSongs < ActiveRecord::Migration[5.0]
+#   def change
+#     remove_column :songs, :album_id
+#   end
+# end
--- db/migrate/20170509184700_remove_relation_from_album.rb
+++ db/migrate/20170509184700_remove_relation_from_album.rb
@@ -1,5 +1,5 @@
-class RemoveRelationFromAlbum < ActiveRecord::Migration[5.0]
-  def change
-    remove_foreign_key :albums, :songs
-  end
-end
+# class RemoveRelationFromAlbum < ActiveRecord::Migration[5.0]
+#   def change
+#     remove_foreign_key :albums, :songs
+#   end
+# end
