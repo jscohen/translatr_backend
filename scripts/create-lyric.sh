34eb8d5a1618ab050e7aba338fe7e3cf9aef9ff1
jscohen <jon.cohen08@gmail.com>
2017-05-12 12:50:01 -0400

    Added create lyric script

--- scripts/create-lyric.sh
+++ scripts/create-lyric.sh
@@ -2,7 +2,7 @@ curl --include --request POST http://localhost:4741/lyrics \
   --header "Content-Type: application/json" \
   --data '{
     "lyrics": {
-      "song_id": "116",
+      "song_id": "153",
       "title": "Thriller",
       "artist": "Michael Jackson"
     }
